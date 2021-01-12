0988cf44e5f1722fc5d1e9ec0b8ebebb015e8b4b
VitaliyAntonov <sinh.project@gmail.com>
2021-01-12 16:39:33 +0500

    Испытано чтение строк из файла в List

--- src/scala_basic.scala
+++ src/scala_basic.scala
@@ -2,7 +2,7 @@
 
 //TODO Книга Scala Профессиональное программирование
 
-
+import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable
 import scala.collection.immutable.HashSet
 import scala.io.Source
@@ -11,8 +11,8 @@ object scala_basic extends App{
   println("Hello World")
 
   nabor // Использование наборов и отображений
-}
 
+}
 
 
 //TODO Шаг 10. Использование наборов и отображений */
@@ -21,18 +21,27 @@ object nabor {
   //TODO  Листинг 3.2. Создание и инициализация массива
   val numNames = Array("zero", "one", "two")
 
+  //TODO Листинг 3.3. Создание и инициализация списка
+  val oneTwoThree = List(1, 2, 3)
+  // Слияние двух списков с образованием нового списка при помощи ::: метода
+  val oneTwo = List(1, 2)
+  val threeFour = List(3, 4)
+  val oneTwoThreeFour = oneTwo ::: threeFour
+  println(oneTwo + " and " + threeFour + " were not mutated.")
+  println("Thus, " + oneTwoThreeFour + " is a new list.")
+  //оператор :: , который произносится cons («конс») добавляет в
+  //начало существующего списка новый элемент и возвращает
+  //получившийся в результате этого список.
+  val twoThree = List(2, 3)
+  val oneTwoThree_ok = 1 :: twoThree
+  println("Метод :: " + oneTwoThree_ok)
 
-//TODO  Листинг 3.5. Создание, инициализация и использование неизменяемого набора */
+  //TODO  Листинг 3.5. Создание, инициализация и использование неизменяемого набора */
   var jetSet = Set("Boeing", "Airbus")
   jetSet += "Lear"
   println(jetSet.contains("Cessna"))
   println(jetSet.contains("Airbus"))
   println(jetSet.contains("Lear"))
-
-
-
-
-
 
 //TODO Листинг 3.6. Создание, инициализация и использование изменяемого набора */
   val movieSet = mutable.Set("Hitch", "Poltergeist")
@@ -68,15 +77,34 @@ object nabor {
 
   //TODO Листинг 3.10. Считывание строк из файла
   // import scala.io.Source
-//  if (args.length > 0) {
-//    for (line <- Source.fromFile(args(0)).getLines())
-//      println(line.length + " " + line)
-//  }
-//  else
-//    Console.err.println("Please enter filename")
+  val args = "/home/vitaly/develop/scalla/Text.txt"
+  var line:String = null
+  if (args.length > 0) {
+    for (line <- Source.fromFile(args).getLines())
+      println(line.length + " " + line)
+  }
+  else
+    Console.err.println("Please enter filename")
 
+  println(fun.readFileLines(args))
 
+  lazy val busFile = fun.readFileLines(args)
+  for(i <- 0 until busFile.length)
+    println(i + " " + busFile(i))
+
+//  line.fromFile(args).foreach {
+//    print
 }
+
+object fun {
+  def readFileLines(path: String): List[String] = { // чтение строк из файла в буфер
+    var mab = List[String]()
+    for (line <- Source.fromFile(path).getLines())  mab += line
+    mab
+  }
+}
+
+
 
 class ChecksumAccumulator {
   private var sum = 0
